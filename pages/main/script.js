da1b02b147d05d633ee862b6548d4af73e282622
Katarzyna Kamieniecka <kamienieckakat@gmail.com>
2022-12-05 14:43:45 +0100

    feat. testimonials interactivity

--- pages/main/script.js
+++ pages/main/script.js
@@ -6,16 +6,26 @@ const nav = document.getElementById("navigation-header-burger");
 const header = document.querySelector(".header");
 const burgerXBtn = document.querySelector(".burger__x");
 
+const addOverlay = () => {
+  overlay.style.display = "block";
+  overlay.addEventListener("click", () => {
+    removeOverlay();
+  });
+};
+
+const removeOverlay = () => {
+  overlay.style.display = "none";
+};
+
 const removeBurger = () => {
   nav.classList.remove("showBurgerMenu");
   header.classList.remove("header--white");
-  overlay.style.display = "none";
 };
 
 const addBurger = () => {
   nav.classList.add("showBurgerMenu");
   header.classList.add("header--white");
-  overlay.style.display = "block";
+  addOverlay();
   overlay.addEventListener("click", removeBurger);
 };
 
@@ -23,4 +33,30 @@ if (screen.width <= 640) {
   burgerXBtn.style.display = "block";
   burgerXBtn.addEventListener("click", removeBurger);
   burgerMenuButton.addEventListener("click", addBurger);
+}
+
+//testimonial popup
+const popupToFill = document.querySelector(".popup-testimonial");
+const allTestimonials = document.querySelectorAll(".testimonial");
+
+const hidePopUp = () => {
+  popupToFill.style.display = "none";
+  removeOverlay();
+};
+
+if (screen.width <= 640) {
+  popupToFill.addEventListener("click", hidePopUp);
+  allTestimonials.forEach((testimonial) => {
+    testimonial.addEventListener("click", () => {
+      popupToFill.style.display = "block";
+      popupToFill.innerHTML = testimonial.innerHTML;
+      const popupClose = document.createElement("button");
+      popupClose.classList.add("btn-x-testimonial");
+      popupClose.innerText = "⨉";
+      popupToFill.appendChild(popupClose);
+      addOverlay();
+      overlay.addEventListener("click", hidePopUp);
+      popupClose.addEventListener("click", hidePopUp);
+    });
+  });
 }
